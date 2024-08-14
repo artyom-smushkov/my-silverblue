d6ae714b7a45f7deaf8c75ae188d93f990263659
Artem Smushkov <artyom.smushkov@gmail.com>
2024-08-14 18:50:06 +0300

    Add docker

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+curl -O https://download.docker.com/linux/fedora/docker-ce.repo
+install -o 0 -g 0 -m644 docker-ce.repo /etc/yum.repos.d/docker-ce.repo
+rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ### Install packages
 
@@ -13,7 +16,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install foot emacs
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
