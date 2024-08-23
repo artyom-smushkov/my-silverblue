2f7a8dcc5322c1f2831e2b6402490f9b0eb67d0b
Artem Smushkov <artyom.smushkov@gmail.com>
2024-08-23 12:21:49 +0300

    Disable mullvad for now

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 curl -O https://download.docker.com/linux/fedora/docker-ce.repo
-curl -O https://repository.mullvad.net/rpm/stable/mullvad.repo
-install -o 0 -g 0 -m644 mullvad.repo /etc/yum.repos.d/mullvad.repo
+# curl -O https://repository.mullvad.net/rpm/stable/mullvad.repo
 install -o 0 -g 0 -m644 docker-ce.repo /etc/yum.repos.d/docker-ce.repo
-rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin mullvad-vpn
+# install -o 0 -g 0 -m644 mullvad.repo /etc/yum.repos.d/mullvad.repo
+rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin #mullvad-vpn
 
 ### Install packages
 
