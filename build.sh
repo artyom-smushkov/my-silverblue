9f1a4ad1323656c8c92f387e1c438b7201137071
Artem Smushkov <artyom.smushkov@gmail.com>
2024-09-09 12:36:27 +0300

    do no install steam, remove solaar

--- build.sh
+++ build.sh
@@ -18,7 +18,8 @@ rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin do
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install foot stow podman-compose syncthing steam
+rpm-ostree install foot stow podman-compose syncthing
+rpm-ostree remove solaar
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
