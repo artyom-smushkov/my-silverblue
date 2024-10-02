82b5fe6380017232576a1c24dc40b1262aa1e761
Artem Smushkov <artyom.smushkov@gmail.com>
2024-10-02 13:08:56 +0300

    Remove conda, add wm stuff

--- build.sh
+++ build.sh
@@ -6,8 +6,12 @@ RELEASE="$(rpm -E %fedora)"
 
 curl -O https://download.docker.com/linux/fedora/docker-ce.repo
 curl -O https://repository.mullvad.net/rpm/stable/mullvad.repo
+curl -O https://copr.fedorainfracloud.org/coprs/lexa/SwayNotificationCenter/repo/fedora-40/lexa-SwayNotificationCenter-fedora-40.repo
+curl -O https://copr.fedorainfracloud.org/coprs/alebastr/sway-extras/repo/fedora-40/alebastr-sway-extras-fedora-40.repo
 install -o 0 -g 0 -m644 docker-ce.repo /etc/yum.repos.d/docker-ce.repo
 install -o 0 -g 0 -m644 mullvad.repo /etc/yum.repos.d/mullvad.repo
+install -o 0 -g 0 -m644 lexa-SwayNotificationCenter-fedora-40.repo /etc/yum.repos.d/lexa-SwayNotificationCenter-fedora-40.rep3o
+install -o 0 -g 0 -m644 alebastr-sway-extras-fedora-40.repo /etc/yum.repos.d/alebastr-sway-extras-fedora-40.repo
 rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ### Install packages
@@ -18,7 +22,7 @@ rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin do
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install foot stow podman-compose syncthing kitty fish emacs conda
+rpm-ostree install foot stow podman-compose syncthing kitty fish emacs hyprland hypridle hyprlock rofi-wayland dunst waybar river
 rpm-ostree override remove solaar
 
 # this would install a package from rpmfusion
