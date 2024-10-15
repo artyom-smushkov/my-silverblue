068878d83bbb51ebbebc451e544bd7e815576e22
Artem Smushkov <artyom.smushkov@gmail.com>
2024-10-15 16:46:57 +0300

    Remove some comments

--- build.sh
+++ build.sh
@@ -18,17 +18,8 @@ rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin do
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
 rpm-ostree install foot stow podman-compose syncthing fish emacs rofi-wayland swww SwayNotificationCenter qtile qtile-wayland qtile-extras alacritty xdg-desktop-portal-wlr swappy dfu-util waybar river
 # rpm-ostree override remove solaar
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
