4fce0a87c4002129d8c4b44bc38f81de9e309eaf
Artem Smushkov <artyom.smushkov@gmail.com>
2024-10-15 16:33:36 +0300

    Switch to sericea base

--- build.sh
+++ build.sh
@@ -34,3 +34,17 @@ rpm-ostree override remove solaar
 
 systemctl enable podman.socket
 systemctl enable docker.service
+
+# Homebrew
+# Convince the installer we are in CI
+touch /.dockerenv
+
+# Make these so script will work
+mkdir -p /var/home
+mkdir -p /var/roothome
+
+# Brew Install Script
+curl -Lo /tmp/brew-install https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+chmod +x /tmp/brew-install
+/tmp/brew-install
+tar --zstd -cvf /usr/share/homebrew.tar.zst /home/linuxbrew/.linuxbrew
