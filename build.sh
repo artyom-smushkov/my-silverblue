5e72dca959d4c5874c7e5854f444dc85c267521a
Artem Smushkov <artyom.smushkov@gmail.com>
2024-10-23 13:24:47 +0300

    Packages cleanup

--- build.sh
+++ build.sh
@@ -6,19 +6,17 @@ RELEASE="$(rpm -E %fedora)"
 
 curl -O https://download.docker.com/linux/fedora/docker-ce.repo
 curl -O https://repository.mullvad.net/rpm/stable/mullvad.repo
-curl -O https://copr.fedorainfracloud.org/coprs/lexa/SwayNotificationCenter/repo/fedora-40/lexa-SwayNotificationCenter-fedora-40.repo
 curl -O https://copr.fedorainfracloud.org/coprs/alebastr/sway-extras/repo/fedora-40/alebastr-sway-extras-fedora-40.repo
 curl -O https://copr.fedorainfracloud.org/coprs/frostyx/qtile/repo/fedora-40/frostyx-qtile-fedora-40.repo
 install -o 0 -g 0 -m644 docker-ce.repo /etc/yum.repos.d/docker-ce.repo
 install -o 0 -g 0 -m644 mullvad.repo /etc/yum.repos.d/mullvad.repo
-install -o 0 -g 0 -m644 lexa-SwayNotificationCenter-fedora-40.repo /etc/yum.repos.d/lexa-SwayNotificationCenter-fedora-40.repo
 install -o 0 -g 0 -m644 alebastr-sway-extras-fedora-40.repo /etc/yum.repos.d/alebastr-sway-extras-fedora-40.repo
 install -o 0 -g 0 -m644 frostyx-qtile-fedora-40.repo /etc/yum.repos.d/frostyx-qtile-fedora-40.repo
 rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ### Install packages
 
-rpm-ostree install foot stow podman-compose syncthing fish emacs rofi-wayland swww SwayNotificationCenter qtile qtile-wayland qtile-extras alacritty xdg-desktop-portal-wlr swappy dfu-util waybar river libvterm
+rpm-ostree install foot stow podman-compose syncthing emacs rofi-wayland swww qtile qtile-wayland qtile-extras xdg-desktop-portal-wlr swappy waybar
 # rpm-ostree override remove solaar
 
 #### Example for enabling a System Unit File
