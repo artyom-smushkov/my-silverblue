e93f997f9d3bf4312b6fc46c2686fc15217bfbde
Artem Smushkov <artyom.smushkov@gmail.com>
2024-11-01 19:36:04 +0200

    Do not install Save Desktop

--- system-files/usr/local/bin/daily-install.sh
+++ system-files/usr/local/bin/daily-install.sh
@@ -9,7 +9,6 @@ flatpak install -y com.rawtherapee.RawTherapee
 flatpak install -y org.rncbc.qpwgraph
 flatpak install -y fm.reaper.Reaper
 flatpak install -y org.gnome.Boxes
-flatpak install -y io.github.vikdevelop.SaveDesktop
 flatpak install -y org.gnome.Geary
 flatpak install -y me.kozec.syncthingtk
 flatpak install -y org.gnome.Lollypop
