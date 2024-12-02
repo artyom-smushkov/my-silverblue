fa80abece52c365ff531625952933c085535b892
Artem Smushkov <artyom.smushkov@gmail.com>
2024-12-02 11:58:56 +0200

    Uptade to 41

--- build.sh
+++ build.sh
@@ -6,12 +6,10 @@ RELEASE="$(rpm -E %fedora)"
 
 curl -O https://download.docker.com/linux/fedora/docker-ce.repo
 curl -O https://repository.mullvad.net/rpm/stable/mullvad.repo
-curl -O https://copr.fedorainfracloud.org/coprs/alebastr/sway-extras/repo/fedora-40/alebastr-sway-extras-fedora-40.repo
-curl -O https://copr.fedorainfracloud.org/coprs/frostyx/qtile/repo/fedora-40/frostyx-qtile-fedora-40.repo
+curl -O https://copr.fedorainfracloud.org/coprs/alebastr/sway-extras/repo/fedora-41/alebastr-sway-extras-fedora-41.repo
 install -o 0 -g 0 -m644 docker-ce.repo /etc/yum.repos.d/docker-ce.repo
 install -o 0 -g 0 -m644 mullvad.repo /etc/yum.repos.d/mullvad.repo
-install -o 0 -g 0 -m644 alebastr-sway-extras-fedora-40.repo /etc/yum.repos.d/alebastr-sway-extras-fedora-40.repo
-install -o 0 -g 0 -m644 frostyx-qtile-fedora-40.repo /etc/yum.repos.d/frostyx-qtile-fedora-40.repo
+install -o 0 -g 0 -m644 alebastr-sway-extras-fedora-41.repo /etc/yum.repos.d/alebastr-sway-extras-fedora-41.repo
 rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ### Install packages
