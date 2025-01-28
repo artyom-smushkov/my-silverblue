39b2b261ac5bcadc271477fd03f7a208a0e82c5e
Artem Smushkov <artyom.smushkov@gmail.com>
2025-01-28 13:04:08 +0200

    More fresh hyprland

--- build.sh
+++ build.sh
@@ -8,10 +8,12 @@ curl -O https://download.docker.com/linux/fedora/docker-ce.repo
 curl -O https://repository.mullvad.net/rpm/stable/mullvad.repo
 curl -O https://copr.fedorainfracloud.org/coprs/alebastr/sway-extras/repo/fedora-41/alebastr-sway-extras-fedora-41.repo
 curl -O https://copr.fedorainfracloud.org/coprs/erikreider/SwayNotificationCenter/repo/fedora-41/erikreider-SwayNotificationCenter-fedora-41.repo
+curl -O https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-41/solopasha-hyprland-fedora-41.repo
 install -o 0 -g 0 -m644 docker-ce.repo /etc/yum.repos.d/docker-ce.repo
 install -o 0 -g 0 -m644 mullvad.repo /etc/yum.repos.d/mullvad.repo
 install -o 0 -g 0 -m644 alebastr-sway-extras-fedora-41.repo /etc/yum.repos.d/alebastr-sway-extras-fedora-41.repo
 install -o 0 -g 0 -m644 erikreider-SwayNotificationCenter-fedora-41.repo /etc/yum.repos.d/erikreider-SwayNotificationCenter-fedora-41.repo
+install -o 0 -g 0 -m644 solopasha-hyprland-fedora-41.repo /etc/yum.repos.d/solopasha-hyprland-fedora-41.repo
 rpm-ostree install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ### Install packages
