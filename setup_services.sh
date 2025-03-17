ccae0bfd893820e929ad63d413db8579a174d2c9
Artem Smushkov <artyom.smushkov@gmail.com>
2025-03-17 19:09:15 +0200

    Enable some services

--- setup_services.sh
+++ setup_services.sh
@@ -5,3 +5,5 @@ set -ouex pipefail
 systemctl enable podman.socket
 systemctl enable docker.service
 systemctl enable brew-setup.service
+systemctl enable mullvad-daemon.service
+systemctl enable lactd.service
