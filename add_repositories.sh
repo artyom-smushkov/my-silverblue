cbdec7d0fb58825c2f99ec1ac1e14a5e25d310c1
Artem Smushkov <artyom.smushkov@gmail.com>
2025-07-21 14:13:05 +0300

    Revert "Cachyos kernel"
    This reverts commit 70bb5fd4b629df949ae71c57fb8b2ba63e048f47.

--- add_repositories.sh
+++ add_repositories.sh
@@ -8,10 +8,8 @@ RELEASE="$(rpm -E %fedora)"
 curl -O https://copr.fedorainfracloud.org/coprs/alebastr/sway-extras/repo/fedora-42/alebastr-sway-extras-fedora-42.repo
 curl -O https://copr.fedorainfracloud.org/coprs/erikreider/SwayNotificationCenter/repo/fedora-42/erikreider-SwayNotificationCenter-fedora-42.repo
 curl -O https://copr.fedorainfracloud.org/coprs/solopasha/hyprland/repo/fedora-42/solopasha-hyprland-fedora-42.repo
-curl -O https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-42/bieszczaders-kernel-cachyos-fedora-42.repo
 
 # install -o 0 -g 0 -m644 docker-ce.repo /etc/yum.repos.d/docker-ce.repo
 install -o 0 -g 0 -m644 alebastr-sway-extras-fedora-42.repo /etc/yum.repos.d/alebastr-sway-extras-fedora-42.repo
 install -o 0 -g 0 -m644 erikreider-SwayNotificationCenter-fedora-42.repo /etc/yum.repos.d/erikreider-SwayNotificationCenter-fedora-42.repo
 install -o 0 -g 0 -m644 solopasha-hyprland-fedora-42.repo /etc/yum.repos.d/solopasha-hyprland-fedora-42.repo
-install -o 0 -g 0 -m644 bieszczaders-kernel-cachyos-fedora-42.repo /etc/yum.repos.d/bieszczaders-kernel-cachyos-fedora-42.repo
